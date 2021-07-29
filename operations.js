3ed5dc697e4dc9dafec8e622111842f3d7fb7066
sureshponnadav2 <suresh.ponnada@v2solutions.com>
2021-07-29 08:23:04 +0530

    modificarions in js file

--- operations.js
+++ operations.js
@@ -2,65 +2,62 @@
 
 
 const operations = {}
-
+let inputValue = document.getElementById('inputId');
+// Addition
 Object.defineProperty(
-    operations, 'addMethod', {
-    value: function () {
-        let inputValue = document.getElementById('inputId');
-        let addBtn = document.getElementById('add');
-        addBtn.addEventListener(
-            'click', function () {
-                inputValue.value = Number(inputValue.value) + 1;
-            }
-        );
-    }
+  operations, 'addMethod', {
+  value: function () {
+    let addBtn = document.getElementById('add');
+    addBtn.addEventListener(
+      'click', function () {
+        inputValue.value = Number(inputValue.value) + 1;
+      }
+    );
+  }
 }
 )
 operations.addMethod();
 
 // Substraction
 Object.defineProperty(
-  operations,'subMethod', {
-      value: function(){
-        let inputValue = document.getElementById('inputId');
-        let subBtn = document.getElementById('sub');
-        subBtn.addEventListener(
-            'click', function(){
-                inputValue.value = Number(inputValue.value) - 1;
-            }
-        )
+  operations, 'subMethod', {
+  value: function () {
+    let subBtn = document.getElementById('sub');
+    subBtn.addEventListener(
+      'click', function () {
+        inputValue.value = Number(inputValue.value) - 1;
       }
+    )
   }
+}
 );
 operations.subMethod()
 
-  // Increment by 10
+// Increment by 10
 Object.defineProperty(
-  operations,'increment', {
-      value: function(){
-        let inputValue = document.getElementById('inputId');
-        let incrBtn = document.getElementById('increment');
-        incrBtn.addEventListener(
-            'click', function(){
-                inputValue.value = Number(inputValue.value) + 10;
-            }
-        )
+  operations, 'increment', {
+  value: function () {
+    let incrBtn = document.getElementById('increment');
+    incrBtn.addEventListener(
+      'click', function () {
+        inputValue.value = Number(inputValue.value) + 10;
       }
+    )
   }
+}
 );
 operations.increment()
 
-   // Decrement by 10
+// Decrement by 10
 Object.defineProperty(
-  operations,'decrement', {
-      value: function(){
-     let inputValue = document.getElementById('inputId');
-     let decrBtn = document.getElementById('decrement');
-     decrBtn.addEventListener('click',function(){
-         inputValue.value = Number(inputValue.value) - 10;
-     });
-      }
+  operations, 'decrement', {
+  value: function () {
+    let decrBtn = document.getElementById('decrement');
+    decrBtn.addEventListener('click', function () {
+      inputValue.value = Number(inputValue.value) - 10;
+    });
   }
+}
 );
 operations.decrement()
 
